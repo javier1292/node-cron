07cfc91203189271d0f39d7b67d79119e3056ea2
Junior Garcia <66648525+javier1292@users.noreply.github.com>
2023-07-22 16:16:35 -0400

    nueva hora

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const shell = require('shelljs');
 
 // Schedule tasks to be run on the server.
-cron.schedule('43 11 * * *', function() {
+cron.schedule('20 16 * * *', function() {
   console.log('Running cronjobs');
   shell.exec('php artisan schedule:run >> /dev/null 2>&1');
 });
