7c2d585b2a66be91edef20320345e58db279563b
Junior Garcia <66648525+javier1292@users.noreply.github.com>
2023-07-22 11:26:10 -0400

    [FEAT] SET UP

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import cron from 'node-cron';
+const cron = require('node-cron');
+const shell = require('shelljs');
 
-cron.schedule(`*/1 * * * *`, async () => {
-  console.log(`running your task...`);
+// Schedule tasks to be run on the server.
+cron.schedule('28 11 * * *', function() {
+  console.log('Running cronjobs');
+  shell.exec('php artisan schedule:run >> /dev/null 2>&1');
 });
